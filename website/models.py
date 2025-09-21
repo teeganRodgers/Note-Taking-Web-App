e404854cde5152d0d0bce4bd46638e01b4d58f8d
Teegan R <teeganrodgers@gmail.com>
2025-09-20 20:22:38 -0500

    Initial Commit

---
+++ website/models.py
@@ -0,0 +1,26 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __init__(self, data, user_id):
+        self.data = data
+        self.user_id = user_id
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200), unique=True)
+    password = db.Column(db.String(200))
+    first_name = db.Column(db.String(200))
+    notes = db.relationship("Note")
+
+    def __init__(self, email, password, first_name):
+        self.email = email
+        self.password = password
+        self.first_name = first_name
